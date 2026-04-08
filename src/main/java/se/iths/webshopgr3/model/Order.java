ea6ffbbcfc46f873f1f74713a53152ad8a88282e
katttiger <katttiger@gmail.com>
2026-04-08 10:01:02 +0200

    Added entities and constructors for said entities.

---
+++ src/main/java/se/iths/webshopgr3/model/Order.java
@@ -0,0 +1,41 @@
+package se.iths.webshopgr3.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "order_items")
+    private List<OrderItem> orderItems;
+
+    @Column(nullable = false, name = "customer")
+    private String username;
+
+    @Column(nullable = false, name = "total_price")
+    private double totalPrice;
+
+    @Column(nullable = false, name = "order_date")
+    private Date orderDate;
+
+    public Order() {
+    }
+
+    public Order(Long id, List<OrderItem> orderItems, String username, double totalPrice, Date orderDate) {
+        this.id = id;
+        this.orderItems = orderItems;
+        this.username = username;
+        this.totalPrice = totalPrice;
+        this.orderDate = orderDate;
+    }
+}
---
+++ src/main/java/se/iths/webshopgr3/model/OrderItem.java
@@ -0,0 +1,35 @@
+package se.iths.webshopgr3.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "order_item")
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false, name = "product_name")
+    private String productName;
+
+    @Column(nullable = false, name = "quantity")
+    private int quantity;
+
+    @Column(nullable = false, name = "price")
+    private double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Long id, String productName, int quantity, double price) {
+        this.id = id;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
