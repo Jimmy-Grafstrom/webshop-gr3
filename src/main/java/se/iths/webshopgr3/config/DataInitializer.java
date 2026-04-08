5b16c2828ae17475ca4dd86674636525e650da1b
Jimmy-Grafstrom <jimmy.grafstrom@gmail.com>
2026-04-08 11:43:30 +0200

    feat: Add security configuration and data initializer for application setup

---
+++ src/main/java/se/iths/webshopgr3/config/DataInitializer.java
@@ -0,0 +1,17 @@
+package se.iths.webshopgr3.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class DataInitializer implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
