c6c1ad922117ca84976dbb3e3c90592987ea9e7d
L.W. <reaktor.utfallen-2z@icloud.com>
2026-04-10 16:44:39 +0200

    Add @TestPropertySource to the SpringBootTest.

--- src/test/java/se/iths/webshopgr3/WebshopGr3ApplicationTests.java
+++ src/test/java/se/iths/webshopgr3/WebshopGr3ApplicationTests.java
@@ -2,8 +2,14 @@ package se.iths.webshopgr3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource (
+    properties={
+            "spring.mail.username=fake@gmail.com",
+        "spring.mail.password=fake"
+    })
 class WebshopGr3ApplicationTests {
 
     @Test
