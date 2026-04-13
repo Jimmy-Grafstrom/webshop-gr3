d5dd8c7f8d9f43bb4b221800d2c8e5b42841d85d
katttiger <katttiger@gmail.com>
2026-04-13 08:00:11 +0200

    Added RegistrationController and register.html. Added validation to AppUser-entity. No tests or data has been added to database.

--- src/main/java/se/iths/webshopgr3/controller/RegistrationController.java
+++ src/main/java/se/iths/webshopgr3/controller/RegistrationController.java
@@ -1,13 +1,13 @@
 package se.iths.webshopgr3.controller;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import se.iths.webshopgr3.model.AppUser;
-import se.iths.webshopgr3.repository.AppUserRepository;
 import se.iths.webshopgr3.service.AppUserService;
 
 @Controller
@@ -22,4 +22,11 @@ public class RegistrationController {
         model.addAttribute("user", new AppUser());
         return "register";
     }
+
+    @PostMapping
+    public String addUser(@ModelAttribute AppUser user) {
+        appUserService.saveUser(user);
+        return "redirect://start";
+    }
+
 }
--- src/main/java/se/iths/webshopgr3/model/AppUser.java
+++ src/main/java/se/iths/webshopgr3/model/AppUser.java
@@ -1,6 +1,9 @@
 package se.iths.webshopgr3.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,9 +16,16 @@ public class AppUser {
     @Column(unique = true, nullable = false)
     private Long id;
 
+
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
+            message = "Email is invalid"
+    )
+    @NotEmpty(message = "You must enter an email-adress")
     @Column(nullable = false, unique = true, name = "username")
     private String username;
 
+    @NotBlank(message = "You must enter a password")
     @Column(nullable = false, name = "password")
     private String password;
 
