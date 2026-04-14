45a7c5015f2b1ef3bbba059a788e4777f9bba1af
katttiger <katttiger@gmail.com>
2026-04-14 13:33:56 +0200

    Started working with ProductController and CartController.

--- src/main/java/se/iths/webshopgr3/controller/ProductController.java
+++ src/main/java/se/iths/webshopgr3/controller/ProductController.java
@@ -1,20 +1,20 @@
 package se.iths.webshopgr3.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import se.iths.webshopgr3.service.ProductService;
 
 @Controller
+@RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
 
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
-
-    @GetMapping("/products")
+    @GetMapping()
     public String showProducts(Model model) {
         model.addAttribute("products", productService.getAllProducts());
         return "products";
