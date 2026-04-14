b368bd2b5beaba4b3252ec12a07c795506bb172c
L.W. <reaktor.utfallen-2z@icloud.com>
2026-04-14 21:00:18 +0200

    Update AppUserService so that it can save encrypted password.

--- src/main/java/se/iths/webshopgr3/service/AppUserService.java
+++ src/main/java/se/iths/webshopgr3/service/AppUserService.java
@@ -1,20 +1,28 @@
 package se.iths.webshopgr3.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import se.iths.webshopgr3.model.AppUser;
 import se.iths.webshopgr3.repository.AppUserRepository;
 
 @Service
+@RequiredArgsConstructor
 public class AppUserService {
 
     private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AppUserService(AppUserRepository appUserRepository) {
-        this.appUserRepository = appUserRepository;
-    }
+
 
     public void saveUser(AppUser user) {
+
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        if(user.getRole()== null){
+            user.setRole("USER");
+        }
 
         appUserRepository.save(user);
     }
