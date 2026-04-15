061eac9bedf149f4aac2dea7c092702a63a6a624
Jimmy-Grafstrom <jimmy.grafstrom@gmail.com>
2026-04-15 15:07:13 +0200

    feat: Implement order creation and order item mapping functionality (only stubs)

--- src/main/java/se/iths/webshopgr3/repository/OrderRepository.java
+++ src/main/java/se/iths/webshopgr3/repository/OrderRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import se.iths.webshopgr3.model.Order;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUsername(String username);
 }
