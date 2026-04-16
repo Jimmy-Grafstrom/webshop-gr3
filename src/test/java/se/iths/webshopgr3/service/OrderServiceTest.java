d50743bde46c1f9a2a7b1dd097b9c099f8d694fb
katttiger <katttiger@gmail.com>
2026-04-16 17:39:14 +0200

    Added tests to OrderServiceTest.

--- src/test/java/se/iths/webshopgr3/service/OrderServiceTest.java
+++ src/test/java/se/iths/webshopgr3/service/OrderServiceTest.java
@@ -1,14 +1,15 @@
 package se.iths.webshopgr3.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import se.iths.webshopgr3.model.AppUser;
-import se.iths.webshopgr3.model.Cart;
-import se.iths.webshopgr3.model.CartItem;
-import se.iths.webshopgr3.model.Product;
+import se.iths.webshopgr3.model.*;
 import se.iths.webshopgr3.repository.OrderRepository;
+
+import java.util.List;
 
 @SpringBootTest
 class OrderServiceTest {
@@ -19,36 +20,72 @@ class OrderServiceTest {
     @Autowired
     private OrderRepository orderRepository;
 
+    private AppUser testUser;
+    private Cart testCart;
+    private Product product;
+
     @BeforeEach
     void setUp() {
+        testUser = new AppUser(1L, "name", "password", true, "user1");
+        testCart = new Cart();
+        product = new Product(1L, "TestProduct", 10, "category", "img_url");
 
     }
 
     @Test
+    @DisplayName("Assert that the creation of order (checkout) works")
     public void testCheckout() {
-        Cart cart = new Cart();
-        Product product = new Product(1L, "TestProduct", 3.55, "category", "img_url");
-        AppUser user = new AppUser(1L, "name", "password", true, "user");
         CartItem cartItem = new CartItem(product);
+        testCart.addProductToCart(cartItem);
+        Order order = orderService.createOrder(testCart, testUser);
 
-//        cart.addProductToCart(cart);
-//        Order order= orderService.createOrder(cart, );
+        Assertions.assertEquals(10L, order.getTotalPrice());
 
     }
 
-    @Test
-    void createOrder() {
-    }
 
     @Test
+    @DisplayName("Orders can be retrieved using the username of the customer")
     void getOrdersByUsername() {
+        testUser.setUsername("test2");
+        Order order = orderService.createOrder(testCart, testUser);
+
+        List<Order> expectedOrder = List.of(order);
+        List<Order> orderReturned = orderService.getOrdersByUsername(testUser.getUsername());
+
+        String expectedUser = expectedOrder.getFirst().getUsername();
+        String recievedUser = orderReturned.getFirst().getUsername();
+
+        Assertions.assertEquals(expectedUser, recievedUser);
     }
 
     @Test
+    @DisplayName("Order is not retrieved due to username being incorrect")
+    void orderIsNotRetrievedDueToInvalidUsername() {
+
+    }
+
+
+    @Test
+    @DisplayName("All orders can be retrieved")
     void getAllOrders() {
+
+        List<Order> ordersRecieved = orderService.getAllOrders();
+        Assertions.assertNotNull(ordersRecieved);
+
+
     }
 
     @Test
+    @DisplayName("An order can be found using its id")
     void getOrderById() {
+
+
+    }
+
+    @Test
+    @DisplayName("Orders with invalid id is not returned")
+    void orderNotReturnedDueToIncorrectId() {
+
     }
 }
