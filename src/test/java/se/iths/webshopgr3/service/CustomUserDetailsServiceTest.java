5e545a47c136951ec94f93b80f057b2714b9c716
katttiger <katttiger@gmail.com>
2026-04-17 15:36:24 +0200

    Added tests to CustomUserDetailsServiceTest.

--- src/test/java/se/iths/webshopgr3/service/CustomUserDetailsServiceTest.java
+++ src/test/java/se/iths/webshopgr3/service/CustomUserDetailsServiceTest.java
@@ -1,17 +1,53 @@
 package se.iths.webshopgr3.service;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import se.iths.webshopgr3.model.AppUser;
 
 @SpringBootTest
 class CustomUserDetailsServiceTest {
 
+    private AppUser testUser;
+
+    @Autowired
+    private AppUserService appUserService;
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
     @BeforeEach
     void setUp() {
+        testUser = new AppUser();
+        testUser.setConsent(true);
+        testUser.setPassword("password");
+        testUser.setUsername("test@test.com");
     }
 
     @Test
-    void loadUserByUsername() {
+    @DisplayName("User is loaded correctly")
+    void loadUserByUsernameSucceeds() {
+        appUserService.saveUser(testUser);
+        UserDetails userReturned = customUserDetailsService.loadUserByUsername(testUser.getUsername());
+
+        Assertions.assertEquals(testUser.getUsername(), userReturned.getUsername());
+
+
+    }
+
+    @Test
+    @DisplayName("User does not exists. Exception is thrown.")
+    void loadUserByUserNameFails() {
+        Assertions.assertThrows(UsernameNotFoundException.class, () -> {
+            customUserDetailsService.loadUserByUsername(testUser.getUsername());
+        });
+
+    }
+
+    @AfterEach
+    void clean() {
+        appUserService.deleteUser(testUser.getUsername());
     }
 }
